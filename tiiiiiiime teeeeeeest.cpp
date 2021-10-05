7b83a811595c927df0146d502bb95c0f75d3dd8e
Hedgr <74436143+Hedgr@users.noreply.github.com>
2021-10-05 08:23:14 -0700

    file
    as usual, run in vis. studio

---
+++ tiiiiiiime teeeeeeest.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+using namespace chrono_literals;
+
+int main()
+{
+	short int repeat = 1;
+	short int seconds = 0;
+	short int minutes = 0;
+	short int hours = 0;
+	short int days = 0;
+	short int weeks = 0;
+	short int months = 0;
+	short int years = 0;
+	//uwu
+
+	while (repeat == 1)
+	{
+		std::this_thread::sleep_for(1000ms);
+		seconds += 1;
+
+		if (seconds == 60)
+		{
+			seconds = 0;
+			minutes += 1;
+		}
+		if (minutes == 60)
+		{
+			minutes = 0;
+			hours += 1;
+		}
+		if (hours == 24)
+		{
+			hours = 0;
+			days += 1;
+		}
+		if (days == 7)
+		{
+			days = 0;
+			weeks += 1;
+		}
+		if (weeks == 3)
+		{
+			weeks = 0;
+			months += 1;
+		}
+		if (months == 12)
+		{
+			months = 0;
+			years += 1;
+		}
+		if (seconds == 15)
+		{
+			cout << "dont stop this i want to see what happens (also yes this is open on purpose)\n";
+		}
+		if (seconds == 30)
+		{
+			cout << "dont stop this i want to see what happens (also yes this is open on purpose)\n";
+		}
+		if (seconds == 45)
+		{
+			cout << "dont stop this i want to see what happens (also yes this is open on purpose)\n";
+		}
+		if (seconds == 0)
+		{
+			cout << "dont stop this i want to see what happens (also yes this is open on purpose)\n";
+		}
+		cout << years;
+		cout << " years ";
+		cout << months;
+		cout << " months ";
+		cout << weeks;
+		cout << " weeks ";
+		cout << days;
+		cout << " days ";
+		cout << hours;
+		cout << " hours ";
+		cout << minutes;
+		cout << " minutes ";
+		cout << seconds;
+		cout << " seconds\n";
+	}
+} 
